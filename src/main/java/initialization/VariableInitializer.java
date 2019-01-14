d52088d18e4aa8f43c32df5388d30751b1110aea
Daan van den Heuvel <d.vandenheuvel@eyefreight.com>
2019-01-14 12:44:53 +0100

    Add InitValue class

--- src/main/java/initialization/VariableInitializer.java
+++ src/main/java/initialization/VariableInitializer.java
@@ -34,11 +34,11 @@ import templateInput.definition.VariableDefinition;
  */
 public class VariableInitializer {
 
-  private static Map<String, String> testNoInit = new HashMap<>();
+  private static Map<String, InitValue> testNoInit = new HashMap<>();
   /** The first value that can be used to initialize the type given by the key of this hashMap. */
-  private static Map<String, String> defaultValue1 = new HashMap<>();
+  private static Map<String, InitValue> defaultValue1 = new HashMap<>();
   /** The second value that can be used to initialize the type given by the key of this hashMap. This value is different from defaultValue1. */
-  private static Map<String, String> defaultValue2 = new HashMap<>();
+  private static Map<String, InitValue> defaultValue2 = new HashMap<>();
   private static Map<String, String> parameterizedVariables = new HashMap<>();
   private static Map<String, String> emptyInit = new HashMap<>();
   private static Set<String> collections = new HashSet<>();
@@ -104,11 +104,11 @@ public class VariableInitializer {
   }
 
   private String init1(String type) {
-    return defaultValue1.get(type);
+    return defaultValue1.get(type).getValue();
   }
 
   private String init2(String type) {
-    return defaultValue2.get(type);
+    return defaultValue2.get(type).getValue();
   }
 
   private List<VariableDefinition> getSubTypes(VariableDefinition var) {
@@ -132,46 +132,46 @@ public class VariableInitializer {
   }
 
   private String getNoInitFor(String type) {
-    return (this.testNoInit.containsKey(type)) ? this.testNoInit.get(type) : "null";
+    return (this.testNoInit.containsKey(type)) ? this.testNoInit.get(type).getValue() : "null";
   }
 
   private void initializeJavaDefaults() {
-    defaultValue1.put("int", "1");
-    defaultValue2.put("int", "2");
-    defaultValue1.put("Integer", "3");
-    defaultValue2.put("Integer", "4");
-    defaultValue1.put("boolean", "true");
-    defaultValue2.put("boolean", "false");
-    defaultValue1.put("Boolean", "false");
-    defaultValue2.put("Boolean", "true");
-    defaultValue1.put("long", "1L");
-    defaultValue2.put("long", "2L");
-    defaultValue1.put("Long", "3L");
-    defaultValue2.put("Long", "4L");
-    defaultValue1.put("double", "1.0");
-    defaultValue2.put("double", "2.0");
-    defaultValue1.put("Double", "3.0");
-    defaultValue2.put("Double", "4.0");
-    defaultValue1.put("float", "1.0");
-    defaultValue2.put("float", "2.0");
-    defaultValue1.put("Float", "3.0");
-    defaultValue2.put("Float", "4.0");
-    defaultValue1.put("String", "\"a\"");
-    defaultValue2.put("String", "\"b\"");
+    defaultValue1.put("int", new InitValue("1"));
+    defaultValue2.put("int", new InitValue("2"));
+    defaultValue1.put("Integer", new InitValue("3"));
+    defaultValue2.put("Integer", new InitValue("4"));
+    defaultValue1.put("boolean", new InitValue("true"));
+    defaultValue2.put("boolean", new InitValue("false"));
+    defaultValue1.put("Boolean", new InitValue("false"));
+    defaultValue2.put("Boolean", new InitValue("true"));
+    defaultValue1.put("long", new InitValue("1L"));
+    defaultValue2.put("long", new InitValue("2L"));
+    defaultValue1.put("Long", new InitValue("3L"));
+    defaultValue2.put("Long", new InitValue("4L"));
+    defaultValue1.put("double", new InitValue("1.0"));
+    defaultValue2.put("double", new InitValue("2.0"));
+    defaultValue1.put("Double", new InitValue("3.0"));
+    defaultValue2.put("Double", new InitValue("4.0"));
+    defaultValue1.put("float", new InitValue("1.0"));
+    defaultValue2.put("float", new InitValue("2.0"));
+    defaultValue1.put("Float", new InitValue("3.0"));
+    defaultValue2.put("Float", new InitValue("4.0"));
+    defaultValue1.put("String", new InitValue("\"a\""));
+    defaultValue2.put("String", new InitValue("\"b\""));
 
     // Special ones
-    defaultValue1.put("LocalDateTime", "LocalDateTime.of(2017, 3, 25, 0, 0)");
-    defaultValue2.put("LocalDateTime", "LocalDateTime.of(2018, 4, 26, 1, 1)");
-    defaultValue1.put("BigDecimal", "BigDecimal.valueOf(5)");
-    defaultValue2.put("BigDecimal", "BigDecimal.valueOf(6)");
-    defaultValue1.put("Date", "Date.from(ZonedDateTime.of(2017, 4, 25, 10, 0, 0, 0, TimeZone.getTimeZone(\"UTC\").toZoneId()).toInstant())");
-    defaultValue2.put("Date", "Date.from(ZonedDateTime.of(2018, 5, 26, 11, 0, 0, 0, TimeZone.getTimeZone(\"UTC\").toZoneId()).toInstant())");
-    defaultValue1.put("Length", "SI.METER");
-    defaultValue2.put("Length", "SI.KILOMETER");
-    defaultValue1.put("Volume", "SI.CUBIC_METRE");
-    defaultValue2.put("Volume", "SI.CUBIC_METRE"); // no alternative
-    defaultValue1.put("Mass", "SI.KILOGRAM");
-    defaultValue2.put("Mass", "SI.KILOGRAM");
+    defaultValue1.put("LocalDateTime", new InitValue("LocalDateTime.of(2017, 3, 25, 0, 0)"));
+    defaultValue2.put("LocalDateTime", new InitValue("LocalDateTime.of(2018, 4, 26, 1, 1)"));
+    defaultValue1.put("BigDecimal", new InitValue("BigDecimal.valueOf(5)"));
+    defaultValue2.put("BigDecimal", new InitValue("BigDecimal.valueOf(6)"));
+    defaultValue1.put("Date", new InitValue("Date.from(ZonedDateTime.of(2017, 4, 25, 10, 0, 0, 0, TimeZone.getTimeZone(\"UTC\").toZoneId()).toInstant())"));
+    defaultValue2.put("Date", new InitValue("Date.from(ZonedDateTime.of(2018, 5, 26, 11, 0, 0, 0, TimeZone.getTimeZone(\"UTC\").toZoneId()).toInstant())"));
+    defaultValue1.put("Length", new InitValue("SI.METER"));
+    defaultValue2.put("Length", new InitValue("SI.KILOMETER"));
+    defaultValue1.put("Volume", new InitValue("SI.CUBIC_METRE"));
+    defaultValue2.put("Volume", new InitValue("SI.CUBIC_METRE")); // no alternative
+    defaultValue1.put("Mass", new InitValue("SI.KILOGRAM"));
+    defaultValue2.put("Mass", new InitValue("SI.KILOGRAM"));
   }
 
   private void initializeJavaEmptyInit() {
@@ -185,18 +185,18 @@ public class VariableInitializer {
   }
 
   private void initializeJavaNoInit() {
-    testNoInit.put("int", "0");
-    testNoInit.put("boolean", "false");
-    testNoInit.put("long", "0L");
-    testNoInit.put("double", "0.0");
-    testNoInit.put("float", "0.0");
-    testNoInit.put("Optional", "Optional.empty()");
-    testNoInit.put("List", "Collections.emptyList()");
-    testNoInit.put("ArrayList", "Collections.emptyList()");
-    testNoInit.put("HashMap", "Collections.emptyMap()");
-    testNoInit.put("Map", "Collections.emptyMap()");
-    testNoInit.put("Set", "Collections.emptySet()");
-    testNoInit.put("HashSet", "Collections.emptySet()");
+    testNoInit.put("int", new InitValue("0"));
+    testNoInit.put("boolean", new InitValue("false"));
+    testNoInit.put("long", new InitValue("0L"));
+    testNoInit.put("double", new InitValue("0.0"));
+    testNoInit.put("float", new InitValue("0.0"));
+    testNoInit.put("Optional", new InitValue("Optional.empty()"));
+    testNoInit.put("List", new InitValue("Collections.emptyList()"));
+    testNoInit.put("ArrayList", new InitValue("Collections.emptyList()"));
+    testNoInit.put("HashMap", new InitValue("Collections.emptyMap()"));
+    testNoInit.put("Map", new InitValue("Collections.emptyMap()"));
+    testNoInit.put("Set", new InitValue("Collections.emptySet()"));
+    testNoInit.put("HashSet", new InitValue("Collections.emptySet()"));
   }
 
   private void initializePrimitiveToObject() {
